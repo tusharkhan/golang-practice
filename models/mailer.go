d9241e3a7855e3024d6324bfa1bb09b1a5da9cd6
tusharkhan <tushar.khan0122@gmail.com>
2024-10-30 16:53:45 +0600

    create mail service

--- models/mailer.go
+++ models/mailer.go
@@ -41,13 +41,39 @@ func (es *EmailService) Send(email Email) error {
 	mail.SetHeader("From", email.From)
 	mail.SetHeader("To", email.To)
 	mail.SetHeader("Subject", email.Subject)
-	mail.SetBody("text/plain", email.PlainText)
-	mail.AddAlternative("text/html", email.HTML)
+
+	switch {
+	case email.PlainText != "" && email.HTML != "":
+		mail.SetBody("text/plain", email.PlainText)
+		mail.AddAlternative("text/html", email.HTML)
+	case email.PlainText != "":
+		mail.SetBody("text/plain", email.PlainText)
+	case email.HTML != "":
+		mail.SetBody("text/html", email.HTML)
+	}
 
 	err := es.dialer.DialAndSend(mail)
 
 	if err != nil {
 		return fmt.Errorf("Error in sending mail")
+	}
+
+	return nil
+}
+
+func (es *EmailService) SendForgetPasswordEmail(to, requestUrl string) error {
+	email := Email{
+		From:      "support@golangpractice.com",
+		To:        to,
+		Subject:   "Forget Password Email",
+		PlainText: "To reset your password please follow the link : " + requestUrl,
+		HTML:      `<p> To reset your password please follow the link : <a href="` + requestUrl + `"> ` + requestUrl + ` </a> </p>`,
+	}
+
+	err := es.Send(email)
+
+	if err != nil {
+		return fmt.Errorf("Error in sending forget password mail to" + to)
 	}
 
 	return nil
